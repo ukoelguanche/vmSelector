07098641c26960eae81aaf83eab5deed7ffbb16e
Melvin Gutiérrez <melvin.gutierrez-rivero@starbreeze.com>
2026-03-03 18:00:35 +0000

    Small cleanup

--- drivers/common.go
+++ drivers/common.go
@@ -9,6 +9,10 @@ const (
 
 var GlobalDisplay *Display
 
+func DrawAnimation(sprite *model.SpriteInstance) {
+	GlobalDisplay.DrawSpriteRect(sprite, sprite.CurrentFrame(), sprite.Position.X, sprite.Position.Y)
+}
+
 func DrawText(text *model.Text) {
 	cursorX := text.Position.X
 
@@ -25,52 +29,13 @@ func DrawText(text *model.Text) {
 
 }
 
-/*
-func DrawSprite(sprite *model.Sprite, sectionName string, name string, X int32, Y int32) {
-	section := sprite.GetSection(sectionName)
-	rect := section.GetSprite(name)
-	GlobalDisplay.DrawSpriteRect(sprite.Bitmap, rect, X, Y)
-}
-
-func DrawSpriteGradient(sprite *model.Sprite, sectionName string, name string, X int32, Y int32, sourceGradient model.Gradient, targetGradient model.Gradient, frameIndex int) {
-	normalizeFrameIndex := int(frameIndex / 5)
-	section := sprite.GetSection(sectionName)
-	rect := section.GetSprite(name)
-	GlobalDisplay.DrawSpriteRectGradient(sprite.Bitmap, rect, X, Y, sourceGradient, targetGradient, normalizeFrameIndex)
-}
-
-func DrawAnimation(sprite *model.Sprite, animationName string, frameIndex int, X int32, Y int32) {
-	normalizeFrameIndex := int(frameIndex / 5)
-	animation := sprite.GetAnimation(animationName)
-	rects := sprite.GetAnimationRects(animation.Section)
-
-	frames := animation.Frames
-
-	rect := rects[frames[normalizeFrameIndex%len(frames)]]
-
-	GlobalDisplay.DrawSpriteRect(sprite.Bitmap, rect, X, Y)
-}
-*/
-
-func DrawAnimation(sprite *model.SpriteInstance) {
-	GlobalDisplay.DrawSpriteRect(sprite, sprite.CurrentFrame(), sprite.Position.X, sprite.Position.Y)
-}
-
-func (d *Display) FillRect(rect model.Rect, color []byte) {
-	for y := 0; y < int(rect.Size.H); y++ {
-		for x := 0; x < int(rect.Size.W); x++ {
-			d.DrawPixel(rect.Point.X+int32(x), rect.Point.Y+int32(y), color)
-		}
-	}
-}
-
-func (d *Display) DrawTextRect(sprite *model.Text, src model.Rect, destX, destY int32) {
+func (d *Display) DrawTextRect(sprite *model.Text, rect model.Rect, destX, destY int32) {
 	bitmap := sprite.Sprite.Bitmap
-	for sy := 0; sy < int(src.Size.H); sy++ {
-		for sx := 0; sx < int(src.Size.W); sx++ {
+	for sy := 0; sy < int(rect.Size.H); sy++ {
+		for sx := 0; sx < int(rect.Size.W); sx++ {
 			// Calculate original position inside bitmap
-			origX := src.Point.X + int32(sx)
-			origY := src.Point.Y + int32(sy)
+			origX := rect.Point.X + int32(sx)
+			origY := rect.Point.Y + int32(sy)
 
 			// Avoid drawing outside bounds
 			if origX < 0 || origX >= bitmap.Size.W || origY < 0 || origY >= bitmap.Size.H {
@@ -90,16 +55,16 @@ func (d *Display) DrawTextRect(sprite *model.Text, src model.Rect, destX, destY 
 	}
 }
 
-func (d *Display) DrawSpriteRect(sprite *model.SpriteInstance, src model.Rect, destX, destY int32) {
+func (d *Display) DrawSpriteRect(sprite *model.SpriteInstance, rect model.Rect, destX, destY int32) {
 	sourcePalette := sprite.Sprite.PaletteSwap.SourcePalette
 	targetPalette := sprite.Sprite.PaletteSwap.TargetPalette
 
 	bitmap := sprite.Sprite.Bitmap
-	for sy := 0; sy < int(src.Size.H); sy++ {
-		for sx := 0; sx < int(src.Size.W); sx++ {
+	for sy := 0; sy < int(rect.Size.H); sy++ {
+		for sx := 0; sx < int(rect.Size.W); sx++ {
 			// Calculate original position inside bitmap
-			origX := src.Point.X + int32(sx)
-			origY := src.Point.Y + int32(sy)
+			origX := rect.Point.X + int32(sx)
+			origY := rect.Point.Y + int32(sy)
 
 			// Avoid drawing outside bounds
 			if origX < 0 || origX >= bitmap.Size.W || origY < 0 || origY >= bitmap.Size.H {
@@ -125,33 +90,6 @@ func (d *Display) DrawSpriteRect(sprite *model.SpriteInstance, src model.Rect, d
 		}
 	}
 }
-
-/*
-	func (d *Display) DrawSpriteRectGradient(sprite *model.Bitmap, src model.Rect, destX, destY int32, sourceGradient model.Gradient, targetGradient model.Gradient, animationIndex int) {
-		for sy := 0; sy < int(src.Size.H); sy++ {
-			for sx := 0; sx < int(src.Size.W); sx++ {
-				origX := src.Point.X + int32(sx)
-				origY := src.Point.Y + int32(sy)
-
-				if origX < 0 || origX >= sprite.Size.W || origY < 0 || origY >= sprite.Size.H {
-					continue
-				}
-
-				srcOff := (origY*sprite.Size.W + int32(sx) + src.Point.X) * 4 // Asegúrate de sumar el offset X correctamente
-				color := sprite.Pixels[srcOff : srcOff+4]
-
-				if color[3] < 128 {
-					continue
-				}
-
-				//colorADibujar := ReplaceGradientColor(color, sourceGradient, targetGradient, animationIndex) // Por defecto el original
-				colorADibujar := color
-
-				d.DrawPixel(destX+int32(sx), destY+int32(sy), colorADibujar)
-			}
-		}
-	}
-*/
 
 func ReplacePalette(color []byte, sourcePalette *model.Palette, targetPalette *model.Palette, animationIndex int) []byte {
 	if sourcePalette == nil || targetPalette == nil {
